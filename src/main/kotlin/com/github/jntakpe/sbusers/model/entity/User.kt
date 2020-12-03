0acd3d29c3ff83e27970b7fb1ba6750fca62d25a
jntakpe <jntakpe@gmail.com>
2020-12-03 20:28:39 +0100

    feat: init User model

---
+++ src/main/kotlin/com/github/jntakpe/sbusers/model/entity/User.kt
@@ -0,0 +1,36 @@
+package com.github.jntakpe.sbusers.model.entity
+
+import com.github.jntakpe.commons.mongo.Identifiable
+import org.bson.types.ObjectId
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class User(
+    val username: String,
+    val email: String,
+    val countryCode: String,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val phoneNumber: String? = null,
+    override val id: ObjectId = ObjectId(),
+) : Identifiable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as User
+
+        if (username != other.username) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return username.hashCode()
+    }
+
+    override fun toString(): String {
+        return "User(username='$username')"
+    }
+}
