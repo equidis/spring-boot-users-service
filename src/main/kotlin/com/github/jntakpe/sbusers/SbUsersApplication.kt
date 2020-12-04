3f17894db51f9882aa75ed295fd54491e9f5bfcb
jntakpe <jntakpe@gmail.com>
2020-12-04 22:19:19 +0000

    feat: scan packages under com.github.jntakpe

--- src/main/kotlin/com/github/jntakpe/sbusers/SbUsersApplication.kt
+++ src/main/kotlin/com/github/jntakpe/sbusers/SbUsersApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
-@SpringBootApplication
 @EnableCaching
+@SpringBootApplication(scanBasePackages = ["com.github.jntakpe"])
 class SbUsersApplication
 
 fun main(args: Array<String>) {
