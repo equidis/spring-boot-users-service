0d0c171da55420d3b3ca53f7db161688422f0316
jntakpe <jntakpe@gmail.com>
2020-12-07 21:01:13 +0100

    feat: add an assertion to check CommonExceptionDto HTTP status

--- src/test/kotlin/com/github/jntakpe/commons/test/ExceptionMatchers.kt
+++ src/test/kotlin/com/github/jntakpe/commons/test/ExceptionMatchers.kt
@@ -1,6 +1,7 @@
 package com.github.jntakpe.commons.test
 
 import com.github.jntakpe.commons.context.CommonException
+import com.github.jntakpe.commons.context.CommonExceptionDto
 import org.assertj.core.api.Assertions.assertThat
 import org.springframework.http.HttpStatus
 import reactor.test.StepVerifier
@@ -12,6 +13,11 @@ fun <T> StepVerifier.Step<T>.expectStatusException(status: HttpStatus, message: 
 fun Throwable.assertStatusException(expectedStatus: HttpStatus, expectedMessage: String? = null) {
     assertThat(this).isInstanceOf(CommonException::class.java)
     this as CommonException
-    assertThat(code).isEqualTo(expectedStatus)
+    assertThat(status).isEqualTo(expectedStatus)
+    expectedMessage?.also { assertThat(message).isEqualTo(it) }
+}
+
+fun CommonExceptionDto.assertStatusException(expectedStatus: HttpStatus, expectedMessage: String? = null) {
+    assertThat(code).isEqualTo(expectedStatus.value())
     expectedMessage?.also { assertThat(message).isEqualTo(it) }
 }
