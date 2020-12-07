6e79f9553624de8528a5f622c39a14917ad8a4d2
jntakpe <jntakpe@gmail.com>
2020-12-07 21:00:49 +0100

    feat: add Bean validation annotations on UserDto.kt class

--- src/main/kotlin/com/github/jntakpe/sbusers/model/dto/UserDto.kt
+++ src/main/kotlin/com/github/jntakpe/sbusers/model/dto/UserDto.kt
@@ -1,9 +1,12 @@
 package com.github.jntakpe.sbusers.model.dto
 
+import org.hibernate.validator.constraints.Length
+import javax.validation.constraints.Email
+
 data class UserDto(
-    val username: String,
-    val email: String,
-    val countryCode: String,
+    @field:Length(min = 3) val username: String,
+    @field:Email val email: String,
+    @field:Length(min = 2, max = 2) val countryCode: String,
     val firstName: String? = null,
     val lastName: String? = null,
     val phoneNumber: String? = null,
