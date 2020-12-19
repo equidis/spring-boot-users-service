1ab99679e8d1178c77c8d2b2932b7a1658aa42ac
jntakpe <jntakpe@gmail.com>
2020-12-19 22:14:29 +0100

    feat: add both findByUsername ok and not found contracts

---
+++ src/test/resources/contracts/findByUsername_notfound.groovy
@@ -0,0 +1,25 @@
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+    request {
+        method 'GET'
+        urlPath("/users") {
+            queryParameters {
+                parameter 'username': 'unknown'
+            }
+        }
+        headers {
+            header(contentType(), applicationJson())
+        }
+    }
+    response {
+        status NOT_FOUND()
+        body(
+                message: $(anyNonEmptyString()),
+                code: 404
+        )
+        headers {
+            header(contentType(), applicationJson())
+        }
+    }
+}
