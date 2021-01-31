64a9338836edd8eef604d71afec8d21a3eebc193
jntakpe <jocelyn.ntakpe@soprasteria.com>
2021-01-31 20:11:20 +0000

    build: bump Google Jib plugin version from 2.7.0 to 2.7.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "2.4.2"
     id("org.springframework.cloud.contract") version "2.2.5.RELEASE"
-    id("com.google.cloud.tools.jib") version "2.7.0"
+    id("com.google.cloud.tools.jib") version "2.7.1"
     kotlin("jvm") version "1.4.21"
     kotlin("plugin.spring") version "1.4.21"
 }
